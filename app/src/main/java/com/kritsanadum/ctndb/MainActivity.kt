5eb5a7073113de90d92823d2c0de07e72de18661
kritsanarongkaew <chosendeveloper@gmail.com>
2020-01-14 10:38:27 +0700

    Develop at 14-01-63

--- app/src/main/java/com/kritsanadum/ctndb/MainActivity.kt
+++ app/src/main/java/com/kritsanadum/ctndb/MainActivity.kt
@@ -3,11 +3,10 @@ package com.kritsanadum.ctndb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Toast
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,17 +18,38 @@ class MainActivity : AppCompatActivity() {
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("message")
 
+        btn_send.setOnClickListener {
+
+            var getMsg = et_message.text.toString().trim()
+            myRef.push().setValue(getMsg)
+            et_message.setText("")
+
+        }
+
+
 //        myRef.setValue("Hello, Computer Software.")
+
+
 
         // Read from the database
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                val value = dataSnapshot.getValue(String::class.java)
 
-                //alert
-                Toast.makeText(applicationContext,value,Toast.LENGTH_LONG).show()
+                var data = dataSnapshot!!.children
+                var arrList = arrayListOf<String>()
+                var adapter = ArrayAdapter(applicationContext ,android.R.layout.simple_list_item_1,arrList)
+                lv_result.adapter = adapter
+
+                data.forEach {
+                    arrList.add(it.value.toString())
+                    //alert
+//                    Toast.makeText(applicationContext,"${it.value}",Toast.LENGTH_LONG).show()
+
+                }
+
+                 adapter = ArrayAdapter(applicationContext ,android.R.layout.simple_list_item_1,arrList)
+                lv_result.adapter = adapter
+
 
             }
 
@@ -37,7 +57,10 @@ class MainActivity : AppCompatActivity() {
                 // Failed to read value
 
             }
+
         })
+
+
 
 
 
